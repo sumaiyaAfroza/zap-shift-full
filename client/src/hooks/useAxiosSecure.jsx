457f06a7aed4bac8cc8081852567a2cc7a3ede4c
sumaiyaAfroza <sumaiya.afroza.99@gmail.com>
2025-07-09 13:47:19 +0600

    added deploy with vercel client & server

--- client/src/hooks/useAxiosSecure.jsx
+++ client/src/hooks/useAxiosSecure.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 const axiosSecure = axios.create({
-    baseURL: `http://localhost:3000`
+    baseURL: `https://zap-shift-full.vercel.app`
 })
 
 const useAxiosSecure = () => {
